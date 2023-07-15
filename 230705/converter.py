c4049e78f96670e0b19a5884a522f0d7c25b72b1
mjkweon17 <mjkweon17@korea.ac.kr>
2023-07-15 19:55:32 +0900

    Fix: modified file name

---
+++ 230705/converter.py
@@ -0,0 +1,22 @@
+import csv
+import json
+
+# JSON 파일 읽기
+with open('users.json', 'r') as json_file:
+    data = json.load(json_file)
+
+users = data['users']
+
+# CSV 파일 쓰기
+with open('users.csv', 'w', newline='') as csv_file:
+    writer = csv.writer(csv_file)
+
+    # 헤더 작성
+    headers = users[0].keys()
+    writer.writerow(headers)
+
+    # 데이터 작성
+    for user in users:
+        writer.writerow(user.values())
+
+print("CSV 파일이 생성되었습니다.")
